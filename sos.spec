roidayan/doca-sosreport

commit aa7ca3bc946c43628a73284d36da2ac40d25ec97

Fix rpm conflict with sos
adding sos to obsolete

adding sos-mlx-cloud-verification.conf to rpm build

sos.spec
@@ -19,7 +19,7 @@ Recommends: python3-magic
 # Mandatory just for uploading to a SFTP server:
 Recommends: python3-requests
 Recommends: python3-pyyaml
-Obsoletes: sos-collector <= 1.9
+Obsoletes: sos-collector <= 1.9, sos <= 4.0
 # For the _tmpfilesdir macro.
 BuildRequires: systemd
 # Mandatory just for uploading to an S3 bucket:
@@ -63,6 +63,7 @@ install -d -m 755 %{buildroot}%{_sysconfdir}/%{sosname}/groups.d
 install -d -m 755 %{buildroot}%{_sysconfdir}/%{sosname}/extras.d
 install -d -m 755 %{buildroot}%{_tmpfilesdir}
 install -m 644 %{sosname}.conf %{buildroot}%{_sysconfdir}/%{sosname}/%{sosname}.conf
+install -m 644 sos-mlx-cloud-verification.conf %{buildroot}%{_sysconfdir}/%{sosname}/sos-mlx-cloud-verification.conf
 install -m 644 tmpfiles/tmpfilesd-sos-rh.conf %{buildroot}%{_tmpfilesdir}/%{sosname}.conf
 
 rm -rf %{buildroot}/usr/config/
@@ -92,6 +93,7 @@ rm -rf %{buildroot}/usr/config/
 %doc AUTHORS README.md
 %license LICENSE
 %config(noreplace) %{_sysconfdir}/sos/sos.conf
+%config(noreplace) %{_sysconfdir}/sos/sos-mlx-cloud-verification.conf
 
 %changelog
 * Fri Jun 21 2024 Pavel Moravec <pmoravec@redhat.com> = 4.7.2

commit c034516b38971474a98b120170fe956d0dc53fca

sos.spec: Obsolete any existing sos or sos-collector package
sos-collector only exists up to version 1.9 so leave as is.
Obsolete any sos package for any version.
Still set provides sos to support any existing 3rd party
packages that could look for sos package.

Fixes: aa7ca3bc946c ("Fix rpm conflict with sos")
Signed-off-by: Roi Dayan <roid@nvidia.com>

sos.spec
@@ -19,7 +19,8 @@ Recommends: python3-magic
 # Mandatory just for uploading to a SFTP server:
 Recommends: python3-requests
 Recommends: python3-pyyaml
-Obsoletes: sos-collector <= 1.9, sos <= 4.0
+Obsoletes: sos-collector <= 1.9, sos
+Provides: sos
 # For the _tmpfilesdir macro.
 BuildRequires: systemd
 # Mandatory just for uploading to an S3 bucket: